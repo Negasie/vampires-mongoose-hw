aa822573fa1e5eb18bca8e059711b52acdde8306
Nick Johnson <Blackjacknick@gmail.com>
2019-04-16 16:44:27 -0600

    Commit 1 - Made a schema

--- vampire_app/app.js
+++ vampire_app/app.js
@@ -1,5 +1,23 @@
 // 1. Require your node modules
+const mongoose = require('mongoose');
+const connectionString = 'mongodb://localhost/test';
+mongoose.connect(connectionString, {
+useNewUrlParser: true,
+useCreateIndex: true
 
+});
+
+mongoose.connection.on('connected', () => {
+  console.log(`Mongoose connected to ${connectionString}`);
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log(`Mongoose connected error ${err}`);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose disconnected');
+});
 // 2. Require your model (and possibly your extra data source);
 
 // 3. Connect your database and collection name
